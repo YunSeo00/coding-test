4662888dcf3e23d31084e5df734bd99f2b4595eb
YunSeo00 <chldbstj00@sookmyung.ac.kr>
2024-04-30 20:08:12 +0900

    [Feat] baekjoon 중앙값 구하기 - heap이용으로 시간복잡도 개선 - O(n) -> O(logn)

--- 백준/b_2696.py
+++ 백준/b_2696.py
@@ -27,39 +27,15 @@ input = sys.stdin.readline
 #         k += 1
 #     print()
 
-
-# 삽입 정렬
-t = int(input())
-
-for _ in range(t):
-    # input
+# utils
+def read_data():
     n = int(input())
     numbers = []
     while len(numbers) < n:
         numbers.extend(list(map(int, input().split())))
-    
-    # initial
-    sort_numbers = [numbers[0]] 
-    mid_num = [numbers[0]]
-    count = 1
-    
-    for i in range(1, n):
-        # insertion sort - 삽입 정렬
-        j = i-1
-        if sort_numbers[j] <= numbers[i]:
-            sort_numbers.append(numbers[i])
-        else:
-            j -= 1
-            while j >= 0:
-                if sort_numbers[j] <= numbers[i]:
-                    break
-                j -= 1
-            sort_numbers.insert(j+1, numbers[i])
-        if i % 2 == 0:
-            count += 1
-            mid_num.append(sort_numbers[i//2])
-            
-    # print
+    return n, numbers
+
+def print_results(count, mid_num):
     print(count)
     k = 0
     while k < len(mid_num):
@@ -68,3 +44,67 @@ for _ in range(t):
         print(mid_num[k], end = ' ')
         k += 1
     print()
+
+# 삽입 정렬
+# t = int(input())
+
+# for _ in range(t):
+    
+#     n, numbers = read_data()
+    
+#     # initial
+#     sort_numbers = [numbers[0]] 
+#     mid_num = [numbers[0]]
+#     count = 1
+    
+#     for i in range(1, n):
+#         # insertion sort - 삽입 정렬
+#         j = i-1
+#         if sort_numbers[j] <= numbers[i]:
+#             sort_numbers.append(numbers[i])
+#         else:
+#             j -= 1
+#             while j >= 0:
+#                 if sort_numbers[j] <= numbers[i]:
+#                     break
+#                 j -= 1
+#             sort_numbers.insert(j+1, numbers[i])
+#         if i % 2 == 0:
+#             count += 1
+#             mid_num.append(sort_numbers[i//2])
+            
+#     print_results(count, mid_num)
+    
+
+# heap 이용
+import heapq
+
+t = int(input())
+
+
+for _ in range(t):
+    count = 0
+    mid_num = []
+    max_heap = []
+    min_heap = []
+    
+    n, numbers = read_data()
+    
+    i = 0
+    for num in numbers:
+        if len(max_heap) == len(min_heap):
+            heapq.heappush(max_heap, -num)
+        else:
+            heapq.heappush(min_heap, num)
+        
+        if len(min_heap) > 0 and -max_heap[0] > min_heap[0]:
+            a = -heapq.heappop(max_heap)
+            b = heapq.heappop(min_heap)
+            heapq.heappush(max_heap, -b)
+            heapq.heappush(min_heap, a)
+        if i % 2 == 0:
+            count += 1
+            mid_num.append(-max_heap[0])
+        i+=1
+            
+    print_results(count, mid_num)
