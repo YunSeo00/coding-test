999e11481028d2b724fb84284760d431d889394b
YunSeo00 <chldbstj00@sookmyung.ac.kr>
2024-05-16 02:19:59 +0900

    [Feat] baekjoon LCS

--- 백준/b_9251.py
+++ 백준/b_9251.py
@@ -1,4 +1,5 @@
 # LCS
+# (수정): 연속된 수열을 찾는 것이 아닌 부분 수열을 찾는 것이기 때문에 이전의 matrix에서 max값을 가져옴.
 
 a_text = input()
 b_text = input()
@@ -13,7 +14,7 @@ for i in range(m):
     for j in range(n):
         if a_text[i] == b_text[j]:
             lcs_matrix[i+1][j+1] = lcs_matrix[i][j] + 1
-        if lcs_matrix[i+1][j+1] > result:
-            result = lcs_matrix[i+1][j+1]
+        else:
+            lcs_matrix[i+1][j+1] = max(lcs_matrix[i+1][j], lcs_matrix[i][j+1])
 
-print(result)
+print(lcs_matrix[m][n])
